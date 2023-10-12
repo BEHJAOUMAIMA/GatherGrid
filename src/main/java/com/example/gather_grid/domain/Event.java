a890c7cb16edbc5f0e5dbc434f7316567876a05d
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-10-12 11:01:31 +0100

    add domain package & create Event Entity without relationships

---
+++ src/main/java/com/example/gather_grid/domain/Event.java
@@ -0,0 +1,105 @@
+package com.example.gather_grid.domain;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Time;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+public class Event {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private Date date;
+    private Time hour;
+    private String place;
+    private String description;
+
+    public Event() {
+    }
+
+    public Event(String name, Date date, Time hour, String place, String description) {
+        this.name = name;
+        this.date = date;
+        this.hour = hour;
+        this.place = place;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Time getHour() {
+        return hour;
+    }
+
+    public void setHour(Time hour) {
+        this.hour = hour;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", date=" + date +
+                ", hour=" + hour +
+                ", place='" + place + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return Objects.equals(id, event.id) && Objects.equals(name, event.name) && Objects.equals(date, event.date) && Objects.equals(hour, event.hour) && Objects.equals(place, event.place) && Objects.equals(description, event.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, date, hour, place, description);
+    }
+}
